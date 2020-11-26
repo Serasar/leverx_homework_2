f8f63984b0249e82971ead08ba96ba6e2bf16d3c
user <you@example.com>
2020-11-26 22:24:56 +0300

    Fixed magic numbers

--- task_2.py
+++ task_2.py
@@ -2,7 +2,10 @@ class Version:
     def __init__(self, version):
         self.version = version
         self.version_int = 0
-        self.STRING_WORTH = {"a": 1, "b": 2, "alpha": -20, "beta": -10, "r": -50}
+        self.STRING_WORTH = {"a": 1, "b": 2, "alpha": -2, "beta": -1, "r": -5}
+        self.MAJOR_WORTH = 100
+        self.MINOR_WORTH = 10
+        self.PATCH_WORTH = 0.1
         self.version_to_int()
 
     def version_to_int(self):
@@ -14,23 +17,27 @@ class Version:
             main_part = self.version
         main_part, suffix = main_part.split("."), suffix.split(".")
         # Process main part
-        self.version_int += int(main_part[0]) * 100
+        # Process major version
+        self.version_int += int(main_part[0]) * self.MAJOR_WORTH
+        # Process minor version
         if int(main_part[1]) < 10:
-            self.version_int += int(main_part[1]) * 10
+            self.version_int += int(main_part[1]) * self.MINOR_WORTH
         else:
             self.version_int += int(main_part[1])
-        for symbol in main_part[2]:
-            if symbol.isalpha():
-                self.version_int += self.STRING_WORTH[symbol] * 0.1
-            else:
-                self.version_int += int(symbol) * 0.1
+        # Process patch version
+        if main_part[2][-1].isalpha():
+            self.version_int += self.STRING_WORTH[main_part[2][-1]] * self.PATCH_WORTH
+            main_part[2] = main_part[2][:-1]
+            self.version_int += int(main_part[2]) * self.PATCH_WORTH
+        else:
+            self.version_int += int(main_part[2]) * self.PATCH_WORTH
         # Process suffix part
         if suffix != [""]:
             for symbol in suffix:
                 if symbol.isalpha():
-                    self.version_int += self.STRING_WORTH[symbol] * 0.1
+                    self.version_int += self.STRING_WORTH[symbol] * self.PATCH_WORTH
                 else:
-                    self.version_int += int(symbol) * 0.1
+                    self.version_int += int(symbol) * self.PATCH_WORTH
 
     def __eq__(self, other):
         return self.version_int == other.version_int
@@ -62,11 +69,9 @@ def main():
     ]
 
     for version_1, version_2 in to_test:
-        Version(version_1).version_to_int
-        Version(version_2).version_to_int
-    assert Version(version_1) < Version(version_2), "le failed"
-    assert Version(version_2) > Version(version_1), "ge failed"
-    assert Version(version_2) != Version(version_1), "neq failed"
+        assert Version(version_1) < Version(version_2), "le failed"
+        assert Version(version_2) > Version(version_1), "ge failed"
+        assert Version(version_2) != Version(version_1), "neq failed"
 
 
 if __name__ == "__main__":
